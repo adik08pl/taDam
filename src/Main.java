369429de1513d31e734d3d6fc1108638c032a290
adik08pl <adik08.email@gmail.com>
2023-02-04 13:58:05 +0100

    masz już wczytywanie pliku i dawania go podzielonego do listy

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> slowa = new ArrayList<>();
+        List<String> listaAleTakaLadna = loadFileContent("C:/Users/Bob/Desktop/do Javy/cool.txt");
+        System.out.println(loadFileContent("C:/Users/Bob/Desktop/do Javy/cool.txt").size());
+        for(int i = 0;i<listaAleTakaLadna.size();i++){
+            List<String> lista = Arrays.asList(listaAleTakaLadna.get(i).split(" "));
+           slowa.addAll(lista);
+           System.out.println(lista);
+        }
+    }
+
+    public static List<String> loadFileContent(String filePath) {
+        try {
+            List<String> result = Files.readAllLines(Path.of(filePath));
+            return result;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+}
