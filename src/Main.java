03ea8ff7c8a0111e55a64c9ebc7b685b7037c681
Adik08 <adik08.eamil@gmail.com>
2023-02-10 20:26:22 +0100

    okazało się że da się do zrobić prościej niż myślałem

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,30 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        List<String> lista = null;
         List<String> slowa = new ArrayList<>();
-        List<String> listaAleTakaLadna = loadFileContent("C:/Users/Bob/Desktop/do Javy/cool.txt");
-        System.out.println(loadFileContent("C:/Users/Bob/Desktop/do Javy/cool.txt").size());
-        for(int i = 0;i<listaAleTakaLadna.size();i++){
-            List<String> lista = Arrays.asList(listaAleTakaLadna.get(i).split(" "));
-           slowa.addAll(lista);
-           System.out.println(lista);
+        Map<String, Integer> mapa = new HashMap<>();
+        int zapamietajLiczbe;
+        List<String> listaAleTakaLadna = loadFileContent("C:/Users/sirk0/Desktop/cos.txt");
+        for(int i = 0;i<listaAleTakaLadna.size();i++) {
+            lista = Arrays.asList(listaAleTakaLadna.get(i).split(" "));
+            for(int j = 0;j<lista.size();j++) {
+                if (slowa.contains(lista.get(j))) {
+                zapamietajLiczbe=mapa.get(lista.get(j));
+                mapa.remove(lista.get(j));
+                mapa.put(lista.get(j),zapamietajLiczbe+1);
+                } else {
+                    slowa.add(lista.get(j));
+                    mapa.put(lista.get(j),1);
+                }
+            }
         }
+        System.out.println(mapa);
+
     }
     public static List<String> loadFileContent(String filePath) {
         try {
